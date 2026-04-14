41813a184fe4d3b49a26f7c020acad9638f25263
Miguel David Ayala Gonzalez <mayala@netlogic.com.py>
2026-04-14 15:56:09 -0300

    cambios report client

--- client_report.py
+++ client_report.py
@@ -71,7 +71,7 @@ def load_model():
 
     payload = {
         "model": MODEL_ID,
-        "context_length": 40000,
+        "context_length": 48000,
         "eval_batch_size": 256,
         "offload_kv_cache_to_gpu": True,
         "echo_load_config": True
