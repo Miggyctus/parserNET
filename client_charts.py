21713b872128cf1e770445d64f7eb43bc79ba4c6
Miguel David Ayala Gonzalez <mayala@netlogic.com.py>
2026-04-20 14:40:52 -0300

    añadido qwen

--- client_charts.py
+++ client_charts.py
@@ -38,7 +38,7 @@ def load_model():
 
     payload = {
         "model": MODEL_ID,
-        "context_length": 45000,
+        "context_length": 30000,
         "eval_batch_size": 256,
         "flash_attention": True,
         "offload_kv_cache_to_gpu": True,
